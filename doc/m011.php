303438a81d690346f72afda391cfc81eaa4197bd
syuulee <yuyu00lee@gmail.com>
2022-10-27 15:55:10 +0900

    2022.10.27

---
+++ doc/m011.php
@@ -0,0 +1,17 @@
+<?php
+include_once(G5_THEME_PATH . '../../../common.php')
+include_once(G5_THEME_PATH . '/head.php');
+?>
+
+<main>
+    <section class="main_visual">
+        <div class="main_slide">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+        </div>
+    </section>
+</main>
+
+<?php
+include_once(G5_THEME_PATH . '/tail.php');
---
+++ tail.php
@@ -0,0 +1,37 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH . '/tail.php');
+    return;
+}
+
+if (G5_COMMUNITY_USE === false) {
+    include_once(G5_THEME_SHOP_PATH . '/shop.tail.php');
+    return;
+}
+?>
+
+
+
+
+<?php
+if (G5_DEVICE_BUTTON_DISPLAY && !G5_IS_MOBILE) { ?>
+<?php
+}
+
+if ($config['cf_analytics']) {
+    echo $config['cf_analytics'];
+}
+?>
+
+
+<footer class="footer">
+    footer
+</footer>
+
+
+</div>
+
+<?php
+include_once(G5_THEME_PATH . "/tail.sub.php");
