9beddebae177b6cd8c4ee7d16589500c841e8e9c
syuulee <yuyu00lee@gmail.com>
2022-11-07 12:03:35 +0900

    2022.11.07. 네비 메뉴 수정

--- doc/nav.php
+++ doc/nav.php
@@ -9,9 +9,9 @@
         <a href="<?php echo G5_THEME_URL ?>/doc/m013.php">제품 및 파이프라인</a>
     </li>
     <li>
-        <a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=qa">미디어</a>
+        <a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=qa">질문답변</a>
     </li>
     <li>
-        <a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=notice">채용</a>
+        <a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=notice">공지사항</a>
     </li>
 </ul>
